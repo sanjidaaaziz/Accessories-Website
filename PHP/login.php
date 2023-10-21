0ca17c39f24ffe6d775a1f55c705cd1c06c79072
Sanjida Aziz <83781242+sanjidaaaziz@users.noreply.github.com>
2023-10-21 13:27:09 +0600

    Add files via upload

--- PHP/login.php
+++ PHP/login.php
@@ -4,45 +4,44 @@
 
 session_start();
 
-if(isset($_POST['submit'])){
+$message = array();
 
-   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-   $email = mysqli_real_escape_string($conn, $filter_email);
-   $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
-   $pass = mysqli_real_escape_string($conn, md5($filter_pass));
+if (isset($_POST['submit'])) {
+    $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $email = mysqli_real_escape_string($conn, $filter_email);
+    $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+    $pass = mysqli_real_escape_string($conn, $filter_pass);
 
-   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('Query failed');
 
+    if (mysqli_num_rows($select_users) > 0) {
+        $row = mysqli_fetch_assoc($select_users);
+        $hashed_password = $row['password'];
 
+        // Use password_verify to check the entered password against the hashed password
+        if (password_verify($pass, $hashed_password)) {
+            // Password is correct
+            $_SESSION['user_name'] = $row['name'];
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_id'] = $row['id'];
 
-if(mysqli_num_rows($select_users) > 0){
-      
-      $row = mysqli_fetch_assoc($select_users);
-
-      if($row['user_type'] == 'admin'){
-
-         $_SESSION['admin_name'] = $row['name'];
-         $_SESSION['admin_email'] = $row['email'];
-         $_SESSION['admin_id'] = $row['id'];
-         header('location:admin_page.php');
-
-      }elseif($row['user_type'] == 'user'){
-
-         $_SESSION['user_name'] = $row['name'];
-         $_SESSION['user_email'] = $row['email'];
-         $_SESSION['user_id'] = $row['id'];
-         header('location:home.php');
-
-      }else{
-         $message[] = 'no user found!';
-      }
-
-   }else{
-      $message[] = 'incorrect email or password!';
-   }
-
+            if ($row['user_type'] == 'admin') {
+                $_SESSION['admin_name'] = $row['name'];
+                $_SESSION['admin_email'] = $row['email'];
+                $_SESSION['admin_id'] = $row['id'];
+                header('Location: admin_page.php');
+            } elseif ($row['user_type'] == 'user') {
+                header('Location: home.php');
+            } else {
+                $message[] = 'Invalid user type!';
+            }
+        } else {
+            $message[] = 'Incorrect email or password!';
+        }
+    } else {
+        $message[] = 'User not found!';
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
